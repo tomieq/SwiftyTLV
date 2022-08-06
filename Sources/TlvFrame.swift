956f04e57c899db9e0b426271b40eb6b5e299a2c
tomieq <tomieq@wp.pl>
2022-08-06 09:40:24 +0200

    Changed TlvFrame to use byte as tag

--- Sources/TlvFrame.swift
+++ Sources/TlvFrame.swift
@@ -9,30 +9,21 @@ import Foundation
 
 // Tag Length Value frame
 public struct TlvFrame {
-    let tag: Data
+    let tag: UInt8
     let value: Data
 
-    var tag8: UInt8 {
-        self.tag[0]
-    }
-
-    init(tag: Data, value: Data) {
+    init(tag: UInt8, value: Data) {
         self.tag = tag
         self.value = value
     }
 
-    init(tag: UInt8, value: Data) {
-        self.tag = tag.data
-        self.value = value
-    }
-
     init(tag: UInt8, value: UInt8) {
-        self.tag = tag.data
+        self.tag = tag
         self.value = value.data
     }
 
     init(tag: UInt8, hexString: String) {
-        self.tag = tag.data
+        self.tag = tag
         self.value = Data(hexString: hexString)
     }
 }
