898f55ac3ddcc9ad4a77ce4db668575b2c8d0e07
tomieq <tomieq@wp.pl>
2025-06-17 13:54:01 +0200

    Added SwiftExtensions dependency

--- Package.swift
+++ Package.swift
@@ -1,20 +1,23 @@
-// swift-tools-version:4.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "SwiftyTLV",
-
     products: [
         .library(
             name: "SwiftyTLV",
             targets: ["SwiftyTLV"])
     ],
     dependencies: [
+        .package(url: "https://github.com/tomieq/SwiftExtensions", branch: "master")
     ],
     targets: [
         .target(
             name: "SwiftyTLV",
+            dependencies: [
+                .product(name: "SwiftExtensions", package: "SwiftExtensions")
+            ],
             path: "Sources"),
         .testTarget(
             name: "SwiftyTLVTests",
