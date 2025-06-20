6c6659a499a748ffcb904ac3f9727dd6f88f3717
tomieq <tomieq@wp.pl>
2025-06-20 15:06:17 +0200

    Show in source code hex values of TagType

--- Sources/TagType.swift
+++ Sources/TagType.swift
@@ -6,39 +6,39 @@
 //
 
 public enum TagType: Int, CaseIterable {
-    case boolean = 1
-    case integer = 2
-    case bitString = 3
-    case octetString = 4
-    case null = 5
-    case objectIdentifier = 6
-    case objectDescriptor = 7
-    case external = 8
-    case real = 9
-    case enumerated = 10
-    case embeddedPdv = 11
-    case utf8String = 12
-    case relativeOid = 13
-    case time = 14
-    case sequence = 16
-    case set = 17
-    case numericString = 18
-    case printableString = 19
-    case teletexString = 20
-    case videotexString = 21
-    case ia5String = 22
-    case utcTime = 23
-    case generalizedTime = 24
-    case graphicString = 25
-    case visibleString = 26
-    case generalString = 27
-    case universalString = 28
-    case characterString = 29
-    case bmpString = 30
-    case date = 31
-    case timeOfDay = 32
-    case dateTime = 33
-    case duration = 34
+    case boolean = 0x01            // 1
+    case integer = 0x02            // 2
+    case bitString = 0x03          // 3
+    case octetString = 0x04        // 4
+    case null = 0x05               // 5
+    case objectIdentifier = 0x06   // 6
+    case objectDescriptor = 0x07   // 7
+    case external = 0x08           // 8
+    case real = 0x09               // 9
+    case enumerated = 0x0A         // 10
+    case embeddedPdv = 0x0B        // 11
+    case utf8String = 0x0C         // 12
+    case relativeOid = 0x0D        // 13
+    case time = 0x0E               // 14
+    case sequence = 0x10           // 16
+    case set = 0x11                // 17
+    case numericString = 0x12      // 18
+    case printableString = 0x13    // 19
+    case teletexString = 0x14      // 20
+    case videotexString = 0x15     // 21
+    case ia5String = 0x16          // 22
+    case utcTime = 0x17            // 23
+    case generalizedTime = 0x18    // 24
+    case graphicString = 0x19      // 25
+    case visibleString = 0x1A      // 26
+    case generalString = 0x1B      // 27
+    case universalString = 0x1C    // 28
+    case characterString = 0x1D    // 29
+    case bmpString = 0x1E          // 30
+    case date = 0x1F               // 31
+    case timeOfDay = 0x20          // 32
+    case dateTime = 0x21           // 33
+    case duration = 0x22           // 34
 }
 
 extension TagType: CustomStringConvertible {
